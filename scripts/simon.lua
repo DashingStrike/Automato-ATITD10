4ac60f14b1983b14b7187ed0d9e7c1f2b0a5e578
KavadATITD <unknown>
2021-06-02 19:50:36 -0700

    Add stat waiting back to simon

--- scripts/simon.lua
+++ scripts/simon.lua
@@ -18,7 +18,7 @@ passDelay = 0;
 refresh = true;
 
 function getPoints()
-clickList = {};
+  clickList = {};
   local was_shifted = lsShiftHeld();
   local is_done = false;
   local mx = 0;
@@ -33,8 +33,7 @@ clickList = {};
     was_shifted = is_shifted;
 
     checkBreak();
-    lsPrint(10, 10, z, 1.0, 1.0, 0xFFFFFFff,
-	    "Adding Points (" .. #clickList .. ")");
+    lsPrint(10, 10, z, 1.0, 1.0, 0xFFFFFFff, "Adding Points (" .. #clickList .. ")");
     local y = 60;
     lsPrintWrapped(5, y, z, lsScreenX - 20, 0.7, 0.7, 0xFFFFFFff, "Hover mouse over a point (to click) and Tap Shift to add point.");
     y = y + 50;
@@ -44,21 +43,21 @@ clickList = {};
       local xOff = (index % 4) * 75;
       local yOff = (index - index%4)/2 * 7;
       lsPrint(10 + xOff, y + yOff, z, 0.5, 0.5, 0xffffffff,
-              "(" .. clickList[i][1] .. ", " .. clickList[i][2] .. ")");
+        "(" .. clickList[i][1] .. ", " .. clickList[i][2] .. ")");
       index = index + 1;
     end
 
     if #clickList > 0 then -- Don't show Next button until at least one point is added
       if ButtonText(10, lsScreenY - 30, z, 80, 0xFFFFFFff, "Next") then
-          is_done = 1;
+        is_done = 1;
       end
 
       if ButtonText(87, lsScreenY - 30, 0, 125, 0xFFFFFFff, "Reset Points") then
-          getPoints();
+        getPoints();
       end
     end
     if ButtonText(200, lsScreenY - 30, 0, 110, 0xFFFFFFff,
-                    "End script") then
+      "End script") then
       error(quit_message);
     end
     lsDoFrame();
@@ -72,13 +71,12 @@ function promptRun()
   local scale = 0.7;
   while not is_done do
     checkBreak();
-    lsPrint(10, 10, 0, scale, scale, 0xffffffff,
-            "Configure Sequence");
+    lsPrint(10, 10, 0, scale, scale, 0xffffffff, "Configure Sequence");
     local y = 60;
     lsPrint(5, y, 0, scale, scale, 0xffffffff, "Passes:");
     count = readSetting("count",count);
     is_done, count = lsEditBox("count", 140, y-5, 0, 50, 30, scale, scale,
-                               0x000000ff, count);
+      0x000000ff, count);
     count = tonumber(count);
     if not count then
       is_done = false;
@@ -102,7 +100,7 @@ function promptRun()
       lsPrint(5, y, 0, scale, scale, 0xffffffff, "Click Delay (ms):");
       clickDelay = readSetting("clickDelay",clickDelay);
       is_done, clickDelay = lsEditBox("delay", 140, y-5, 0, 50, 30, scale, scale,
-                                      0x000000ff, clickDelay);
+        0x000000ff, clickDelay);
       clickDelay = tonumber(clickDelay);
       if not clickDelay then
         is_done = false;
@@ -111,42 +109,34 @@ function promptRun()
       end
       writeSetting("clickDelay",clickDelay);
 
-    y = y + 28
-    lsPrint(5, y, 0, scale, scale, 0xffffffff, "Pass Delay (s):");
-    passDelay = readSetting("passDelay",passDelay);
-    is_done, passDelay = lsEditBox("passDelay", 140, y, 0, 50, 30, scale, scale,
-                               0x000000ff, passDelay);
-    passDelay = tonumber(passDelay);
-    if not passDelay then
-      is_done = false;
-      lsPrint(10, y+30, 10, 0.7, 0.7, 0xFF2020ff, "MUST BE A NUMBER");
-      passDelay = 0;
-    end
+      y = y + 28
+      lsPrint(5, y, 0, scale, scale, 0xffffffff, "Pass Delay (s):");
+      passDelay = readSetting("passDelay",passDelay);
+      is_done, passDelay = lsEditBox("passDelay", 140, y, 0, 50, 30, scale, scale,
+        0x000000ff, passDelay);
+      passDelay = tonumber(passDelay);
+      if not passDelay then
+        is_done = false;
+        lsPrint(10, y+30, 10, 0.7, 0.7, 0xFF2020ff, "MUST BE A NUMBER");
+        passDelay = 0;
+      end
 
-
-
-
-    lsPrint(200, y+6, 0, 0.6, 0.6, 0xffffffff, math.floor(passDelay*1000) .. " (ms)");
-    writeSetting("passDelay",passDelay);
-    y = y + 48;
-    lsPrintWrapped(5, y, z, lsScreenX - 20, 0.65, 0.65, 0xFFFFFFff, "Pass Delay: How long to wait, after each click sequence, before moving onto the next pass.");
-else
---    lsPrint(10, 180, z, 0.7, 0.7, 0xFFFFFFff,
---	    "Uncheck Wait for Status Button");
-
-lsPrintWrapped(10, 170, z, lsScreenX - 20, 0.67, 0.67, 0xFFFFFFff, "Unchecking 'Wait for Stats' checkbox will present options for Click Delay and Pass Delay");
-
-
+      lsPrint(200, y+6, 0, 0.6, 0.6, 0xffffffff, math.floor(passDelay*1000) .. " (ms)");
+      writeSetting("passDelay",passDelay);
+      y = y + 48;
+      lsPrintWrapped(5, y, z, lsScreenX - 20, 0.65, 0.65, 0xFFFFFFff, "Pass Delay: How long to wait, after each click sequence, before moving onto the next pass.");
+    else
+      lsPrintWrapped(10, 170, z, lsScreenX - 20, 0.67, 0.67, 0xFFFFFFff, "Unchecking 'Wait for Stats' checkbox will present options for Click Delay and Pass Delay");
     end
 
     if ButtonText(10, lsScreenY - 30, 0, 80, 0xFFFFFFff, "Begin") then
-        is_done = 1;
+      is_done = 1;
     end
     if ButtonText(87, lsScreenY - 30, 0, 125, 0xFFFFFFff, "Reset Points") then
-        getPoints();
+      getPoints();
     end
     if ButtonText(200, lsScreenY - 30, 0, 110, 0xFFFFFFff,
-                    "End script") then
+      "End script") then
       error(quit_message);
     end
     lsSleep(50);
@@ -158,11 +148,11 @@ end
 function clickSequence(count)
   message = "";
   for i=1,count do
-  clickedPoints = "";
+    clickedPoints = "";
 
-  if refresh then
-    refreshWindows();
-  end
+    if refresh then
+      refreshWindows();
+    end
 
     for j=1,#clickList do
       checkBreak();
@@ -171,17 +161,17 @@ function clickSequence(count)
       message = "Pass " .. i .. "/" .. count .. " -- ";
       message = message .. "Clicked " .. j .. "/" .. #clickList .. "\n\n"  .. clickedPoints;
       if is_stats then
-	sleepWithStatus(150, message .. "\nWaiting between clicks", nil, 0.67);
-	  closePopUp(); -- Check for lag/premature click that might've caused a popup box (You're too tired, wait xx more seconds)
-        --waitForStats(message .. "Waiting For Stats");
+        sleepWithStatus(150, message .. "\nWaiting between clicks", nil, 0.67);
+        closePopUp(); -- Check for lag/premature click that might've caused a popup box (You're too tired, wait xx more seconds)
+        waitForStats(message .. "Waiting For Stats");
       else
         sleepWithStatus(clickDelay, message .. "\nWaiting Click Delay", nil, 0.67);
       end
     end
-		--if passDelay > 0 and not is_stats and (i < count) then  --Uncomment so you don't have to wait the full passDelay countdown on last pass; script exits on last pass immediately .
-		if passDelay > 0 and not is_stats then -- No need for passDelay timer if it's 0 or we're using Wait for Stats option
-	        sleepWithStatus(math.floor(passDelay*1000), message .. "\nWaiting on Pass Delay: " .. passDelay .. "s  (" .. math.floor(passDelay*1000) .. " ms)" , nil, 0.67);
-		end
+    --if passDelay > 0 and not is_stats and (i < count) then  --Uncomment so you don't have to wait the full passDelay countdown on last pass; script exits on last pass immediately .
+    if passDelay > 0 and not is_stats then -- No need for passDelay timer if it's 0 or we're using Wait for Stats option
+      sleepWithStatus(math.floor(passDelay*1000), message .. "\nWaiting on Pass Delay: " .. passDelay .. "s  (" .. math.floor(passDelay*1000) .. " ms)" , nil, 0.67);
+    end
   end
   lsPlaySound("Complete.wav");
 end
@@ -202,27 +192,30 @@ function doit()
   end
 end
 
---function waitForStats(message)
---  local stats = findStats();
---  while not stats do
---    sleepWithStatus(500, message, 0xff3333ff);
---    stats = findStats();
---  end
---end
+function waitForStats(message)
+  local stats = findStats();
+  while not stats do
+    sleepWithStatus(500, message, 0xff3333ff);
+    stats = findStats();
+  end
+end
 
---function findStats()
---  srReadScreen();
---  local stats = srFindImage("AllStats-Black.png");
---  if not stats then
---    --stats = srFindImage("AllStats-Black2.png");
---    stats = srFindImage("endurance2.png"); -- If using endurance, we can click early (before all white) on semi-dark red
---  end
---  if not stats then
---    --stats = srFindImage("AllStats-Black3.png");
---    stats = srFindImage("endurance3.png"); -- If using endurance, we can click early (before all white) on dark red
---  end
---  return stats;
---end
+function findStats()
+  srReadScreen();
+  local endurance = srFindImage("stats/endurance.png");
+  if endurance then
+    return false;
+  end
+  local focus = srFindImage("stats/focus.png");
+  if focus then
+    return false;
+  end
+  local strength = srFindImage("stats/strength.png");
+  if strength then
+    return false;
+  end
+  return true;
+end
 
 function closePopUp()
   srReadScreen()
@@ -236,10 +229,10 @@ function refreshWindows()
   statusScreen("Refreshing Windows ...", nil, 0.7);
   srReadScreen();
   pinWindows = findAllImages("ThisIs.png");
-	for i=1, #pinWindows do
-	  checkBreak();
-	  safeClick(pinWindows[i][0], pinWindows[i][1]);
-	  lsSleep(100);
-  	end
+  for i=1, #pinWindows do
+    checkBreak();
+    safeClick(pinWindows[i][0], pinWindows[i][1]);
+    lsSleep(100);
+  end
   lsSleep(500);
 end
