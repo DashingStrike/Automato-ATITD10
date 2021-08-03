1301619ba44e77a0f7a83e152b4c3bc4391d8489
Tribisha <85391832+Tribisha@users.noreply.github.com>
2021-08-03 14:49:14 +1000

    Rename global variable
    Changed num_loops to paper_passes

--- scripts/paper_press.lua
+++ scripts/paper_press.lua
@@ -7,14 +7,14 @@ dofile("settings.inc");
 askText = singleLine([[
   Automatically runs many paper presses, adding/removing linen as necessary. Make sure the
   Automato window is in the TOP-RIGHT corner of the screen. 
-  \n \nPin Drying Hammocks if you want the macro to dry linen each pass.
-  \n \nYou may need to manually adjust Drying Hammock positions if having problems
+  Pin Drying Hammocks if you want the macro to dry linen each pass.
+  You may need to manually adjust Drying Hammock positions if having problems
 ]])
 
 wmText = "Tap Ctrl on paper presses to open and pin.\nTap Alt on paper presses to open, pin and stash.";
 do_dry = readSetting("do_dry",do_dry);
 
-num_loops = readSetting("num_loops",num_loops);
+paper_passes = readSetting("paper_passes",paper_passes);
 ----------------------------------------
 
 function doit()
@@ -37,16 +37,16 @@ function config()
 		lsPrint(10, 10, z, scale, scale, 0xFFFFFFff, "Configure Paper Making Options:");
 		local y = 40;
 
-		num_loops = readSetting("num_loops",tonumber(num_loops));
+		paper_passes = readSetting("paper_passes",tonumber(paper_passes));
 		lsPrint(10, y, z, scale, scale, 0xffffffff, "Passes:");
-		is_done, num_loops = lsEditBox("num_loops", 100, y, z, 50, 30, scale, scale,
-									   0x000000ff, num_loops);
-		if not tonumber(num_loops) then
+		is_done, paper_passes = lsEditBox("paper_passes", 100, y, z, 50, 30, scale, scale,
+									   0x000000ff, paper_passes);
+		if not tonumber(paper_passes) then
 		  is_done = false;
 		  lsPrint(10, y+30, z+10, 0.7, 0.7, 0xFF2020ff, "MUST BE A NUMBER");
-		  num_loops = 1;
+		  paper_passes = 1;
 		end
-		writeSetting("num_loops",tonumber(num_loops));
+		writeSetting("paper_passes",tonumber(paper_passes));
 		y = y + 35;
 
 		do_dry = CheckBox(10, y, z, 0xFFFFFFff, " Dry linen?", do_dry, 0.65, 0.65);
@@ -96,15 +96,15 @@ function dryLinen()
 end
 
 function doPaper()
-	for i=1, num_loops do
+	for i=1, paper_passes do
 
 		-- refresh windows
 		refreshWindows();
 		lsSleep(200);
     makePaper(1);
-		sleepWithStatus(75000, "[" .. i .. "/" .. num_loops .. "] Waiting for 1st batch of paper to finish", nil, 0.7);
+		sleepWithStatus(75000, "[" .. i .. "/" .. paper_passes .. "] Waiting for 1st batch of paper to finish", nil, 0.7);
     makePaper(2);
-		sleepWithStatus(75000, "[" .. i .. "/" .. num_loops .. "] Waiting for 2nd batch of paper to finish", nil, 0.7);
+		sleepWithStatus(75000, "[" .. i .. "/" .. paper_passes .. "] Waiting for 2nd batch of paper to finish", nil, 0.7);
     removeWetLinen();
     lsSleep(200);
     if do_dry then
