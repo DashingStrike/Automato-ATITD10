d8e6cabbff1bd0842f79ae3ed5b9eca1e9363463
Tribisha <85391832+Tribisha@users.noreply.github.com>
2021-08-07 20:23:03 +1000

    Update freeChariot.lua
    Bug fix.

--- scripts/freeChariot.lua
+++ scripts/freeChariot.lua
@@ -8,37 +8,37 @@ function doit()
 
 	  startTime = lsGetTimer();
 	  while 1 do
-      srReadScreen();
-      window = findText("Travel will be free");
-      dest = findText("Travel to");
-      if dest then
-        chariot = string.match(dest[2], "Travel to (%a+)");
-      end
-      travelFree = findText("Travel now for free");
+			srReadScreen();
+			window = findText("Travel will be free");
+			dest = findText("Travel to");
+			if dest then
+			  chariot = string.match(dest[2], "Travel to (%a+)");
+			end
+			travelFree = findText("Travel now for free");
 
-      if window then
-        message = "Waiting for travel to be free ...";
-        safeClick(window[0]+10, window[1]+2);
-      else
-        message = "Could not find Chariot window";
-      end
+			if window then
+			  message = "Waiting for travel to be free ...";
+			  safeClick(window[0]+10, window[1]+2);
+			else
+			  message = "Could not find Chariot window";
+			end
 
-      if travelFree then
-        safeClick(travelFree[0]+10, travelFree[1]+2);
-        break;
-      end
+			if travelFree then
+			  safeClick(travelFree[0]+10, travelFree[1]+2);
+			  break;
+			end
 
-      sleepWithStatus(999, "Traveling to " .. chariot .. "\n\n" .. message .. "\n\nElapsed Time: " .. getElapsedTime(startTime), nil, 0.7, "Monitoring / Refreshing Chariot")
-      lsPlaySound("trolley.wav");
+			sleepWithStatus(999, "Traveling to " .. chariot .. "\n\n" .. message .. "\n\nElapsed Time: " .. getElapsedTime(startTime), nil, 0.7, "Monitoring / Refreshing Chariot")
 
-      -- Add prompt to continue using Free Chariot macro
-      if promptOkay("You have arrived at " .. chariot .. " !\n\nElapsed Time: " .. 
-                     getElapsedTime(startTime) .. "\n\nDo you want to keep traveling?", nil, 0.7, true, true) == nil then
-        lsPlaySound("complete.wav");
-        error("Journey completed. Have a great day!");
-      else
-        sleepWithStatus(2000,"Starting over...",nil, 0.7,nil);
-      end
-    end
+	    -- Add prompt to continue using Free Chariot macro
+	  end
+	   	lsPlaySound("trolley.wav");
+			if promptOkay("You have arrived at " .. chariot .. " !\n\nElapsed Time: " .. 
+				             getElapsedTime(startTime) .. "\n\nDo you want to keep traveling?", nil, 0.7, true, false) == nil then
+				lsPlaySound("complete.wav");
+	      error("Journey completed. Have a great day!");
+	    else
+	    	sleepWithStatus(2000,"Starting over...",nil, 0.7,nil);
+	    end
   end
 end 
